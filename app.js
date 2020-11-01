dcb909237b04be313c5f3fe90d3ba60d4a4828dd
Victor <rusuvictor30@gmail.com>
2020-11-01 15:58:22 -0500

    Level 2

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-window.onload = () => {
-    let start, end = '';
 
     var map = L.map('map', {
         layers: MQ.mapLayer(),
@@ -9,12 +7,15 @@ window.onload = () => {
 
     var dir = MQ.routing.directions();
 
-    dir.route({
-        locations: [
-            start,
-            end
-        ]
-    });
+    function runDirection(start, end) {
+        console.log(start);
+        dir.route({
+            locations: [
+                start,
+                end
+            ]
+        });
+    }
 
     CustomRouteLayer = MQ.Routing.RouteLayer.extend({
         createStartMarker: (location) => {
@@ -54,15 +55,17 @@ window.onload = () => {
         directions: dir,
         fitBounds: true
     }));
-}
+
 
 
 // function that runs when form submitted
 function submitForm(event) {
     event.preventDefault();
-    const formData = new FormData(event.target.value);
-    console.log(formData.get('destination'));
-    
+    start = document.getElementById("start").value;
+    end = document.getElementById("destination").value;
+    // console.log(end);
+    runDirection(start, end);
+    document.getElementById("form").reset();
 }
 
 // asign the form to form variable
