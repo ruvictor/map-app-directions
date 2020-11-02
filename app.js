49ca4d538fe5e0174933ba9db0c10ae5b539daed
Victor <rusuvictor30@gmail.com>
2020-11-01 21:52:03 -0500

    Final

--- app.js
+++ app.js
@@ -1,74 +1,91 @@
-
-    var map = L.map('map', {
-        layers: MQ.mapLayer(),
-        center: [35.791188, -78.636755],
-        zoom: 12
-    });
-
-    var dir = MQ.routing.directions();
+// default map layer
+let map = L.map('map', {
+    layers: MQ.mapLayer(),
+    center: [35.791188, -78.636755],
+    zoom: 12
+});
+    
 
     function runDirection(start, end) {
-        console.log(start);
+        
+        // recreating new map layer after removal
+        map = L.map('map', {
+            layers: MQ.mapLayer(),
+            center: [35.791188, -78.636755],
+            zoom: 12
+        });
+        
+        var dir = MQ.routing.directions();
+
         dir.route({
             locations: [
                 start,
                 end
             ]
         });
+    
+
+        CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+            createStartMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/red.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            },
+
+            createEndMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/blue.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            }
+        });
+        
+        map.addLayer(new CustomRouteLayer({
+            directions: dir,
+            fitBounds: true
+        })); 
     }
-
-    CustomRouteLayer = MQ.Routing.RouteLayer.extend({
-        createStartMarker: (location) => {
-            var custom_icon;
-            var marker;
-
-            custom_icon = L.icon({
-                iconUrl: 'img/red.png',
-                iconSize: [20, 29],
-                iconAnchor: [10, 29],
-                popupAnchor: [0, -29]
-            });
-
-            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
-
-            return marker;
-        },
-
-        createEndMarker: (location) => {
-            var custom_icon;
-            var marker;
-
-            custom_icon = L.icon({
-                iconUrl: 'img/blue.png',
-                iconSize: [20, 29],
-                iconAnchor: [10, 29],
-                popupAnchor: [0, -29]
-            });
-
-            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
-
-            return marker;
-        }
-    });
-
-    map.addLayer(new CustomRouteLayer({
-        directions: dir,
-        fitBounds: true
-    }));
-
 
 
 // function that runs when form submitted
 function submitForm(event) {
     event.preventDefault();
+
+    // delete current map layer
+    map.remove();
+
+    // getting form data
     start = document.getElementById("start").value;
     end = document.getElementById("destination").value;
-    // console.log(end);
+
+    // run directions function
     runDirection(start, end);
+
+    // reset form
     document.getElementById("form").reset();
 }
 
 // asign the form to form variable
 const form = document.getElementById('form');
+
 // call the submitForm() function when submitting the form
 form.addEventListener('submit', submitForm);
